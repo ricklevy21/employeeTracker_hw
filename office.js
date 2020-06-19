9b1d31fa0cc5be80df64ae406044c75c7e2f3eaf
Rick Levy <ricklevy21@gmail.com>
2020-06-19 09:25:07 -0600

    first inquirer message set up

--- office.js
+++ office.js
@@ -15,7 +15,61 @@ connection.connect(function(err){
     if (err) throw err;
     console.log("The connection to the database was made sucessfully");
     //function to start CLI application
+    employeeTracker();
 });
+
+//FUNCTIONS
+
+var employeeTracker = function(){
+    inquirer
+        .prompt({
+            name: "start",
+            type: "list",
+            message: "Greetings. What would you like to do?",
+            choices: [
+                "Add records to database",
+                "View records in database",
+                "Update records in database",
+                "EXIT"
+            ]
+        })
+        .then(function(answer){
+            switch(answer.start){
+                case "Add records to database":
+                    addRecords();
+                    break;
+
+                case "View records in database":
+                    viewRecords();
+                    break;
+
+                case "Update records in database":
+                    updateRecords();
+                    break;
+
+                case "EXIT":
+                    connection.end();
+                    console.log("Thank you, goodbye.")
+            }
+        })
+};
+
+
+var addRecords = function(){
+    console.log("add records")
+};
+
+var viewRecords = function(){
+    console.log("viewRecords")
+};
+
+var updateRecords = function(){
+    console.log("update records")
+};
+
+
+
+
 
 
 
